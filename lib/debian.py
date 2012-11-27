e47268fa545a8c005af602b8ae4cd07177be0d24
Brad Figg <brad.figg@canonical.com>
2012-11-27 11:24:34 -0800

    [digest] Modify the kernel version regex so we can handle mainline builds (and determine a 'series')
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- lib/debian.py
+++ lib/debian.py
@@ -5,6 +5,6 @@ from re                                 import compile
 
 class Debian:
     version_line_rc = compile("^(linux[-\S]*) \(([0-9]+\.[0-9]+\.[0-9]+[-\.][0-9]+\.[0-9]+[~\S]*)\) (\S+); urgency=\S+$")
-    version_rc      = compile("^([0-9]+\.[0-9]+\.[0-9]+)[-\.]([0-9]+)\.([0-9]+)([~\S]*)$")
+    version_rc      = compile("^(\d+\.\d+)\.\d+[-\.](\d+)\.(\d+)([~\S]*)$")
 
 # vi:set ts=4 sw=4 expandtab:
