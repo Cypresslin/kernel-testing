6c283856142154adc1645ed92cc10a6e99506490
Brad Figg <brad.figg@canonical.com>
2014-09-17 12:08:19 -0700

    [lib/configuration.py] Added comments for various pieces
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- lib/configuration.py
+++ lib/configuration.py
@@ -1,18 +1,32 @@
+#!/usr/bin/env python
+#
+
 Configuration = {
+
+    # The Jenkins server section. My Jenkins server is named 'jenkins'.
+    #
     "jenkins" : {
         "server_url" : "http://10.98.2.20:8080"
     },
 
+    # I only have one MAAS server and it's named 'maas'.
+    #
     "maas" : {
-        "server"  : "maas",
-        "profile" : "jenkins",
+        "server"  : "maas",          # Hostname of this maas server.
+        "profile" : "jenkins",       # When talking to the maas server use this profile.
         "creds"   : "VxFpPVkTJ8fkH6MqCq:GC9QjEr4GFaHe2dFcC:eWELKWXvVpTRa8Vzkb9CajVVH2vhXTv9",
-        "user"    : "jenkins",
-        "type"    : "maas"
+        "user"    : "jenkins",       # When ssh'ing to the maas server use this user
+        "type"    : "maas"           # This _is_ a MAAS server (as opposed to a cobbler server)
     },
 
+    # I don't have a cobbler server so I could just leave this section
+    # out. However, if I did have a cobbler server it would be named
+    # 'cobbler'.
+    #
     "cobbler" : {
         "server"  : "cobbler",
         "type"    : "cobbler"
     }
 }
+
+# vi:set ts=4 sw=4 expandtab syntax=python:
