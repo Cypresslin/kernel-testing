7f70a67581c31b92a583201849c32c561dd7daa8
Brad Figg <brad.figg@canonical.com>
2015-09-10 09:04:54 -0700

    [md/*] maas daily image messages that will kick off tests
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ md/vivid_arm64.py
@@ -0,0 +1,9 @@
+
+Msg = {
+    "key"         : "kernel.maas.daily",
+    'systems'     : ['ms10-34-mcdivittB0'],
+    'arches'      : ['arm64'],
+    'series-name' : 'vivid',
+}
+
+# vi:set ts=4 sw=4 syntax=python expandtab:
