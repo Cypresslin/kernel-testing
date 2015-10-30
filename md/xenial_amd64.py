d2763829baced9ad051ce8c772b12c080545f983
Brad Figg <brad.figg@canonical.com>
2015-10-30 07:36:28 -0700

    xenial*: For maas daily image testing
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ md/xenial_amd64.py
@@ -0,0 +1,9 @@
+
+Msg = {
+    "key"         : "kernel.maas.daily",
+    'systems'     : ['fozzie'],
+    'arches'      : ['amd64'],
+    'series-name' : 'xenial',
+}
+
+# vi:set ts=4 sw=4 syntax=python expandtab:
---
+++ md/xenial_ppc64el.py
@@ -0,0 +1,9 @@
+
+Msg = {
+    "key"         : "kernel.maas.daily",
+    'systems'     : ['modoc'],
+    'arches'      : ['ppc64el'],
+    'series-name' : 'xenial',
+}
+
+# vi:set ts=4 sw=4 syntax=python expandtab:
