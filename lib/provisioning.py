d45a0b322f636f2a6824ebfd10f24ffc94dad9a3
Po-Hsu Lin <po-hsu.lin@canonical.com>
2017-03-17 19:11:30 +0800

    provisioning.py: fix arch verification for s390x
    We expect to see the arch name like s390x.zVM, but it will
    only return s390x from 'uname -p'. Add exception to fix it.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- lib/provisioning.py
+++ lib/provisioning.py
@@ -676,6 +676,10 @@ class Metal(Base):
             #
             elif s.arch == 'ppc64el' and installed_arch == 'ppc64le':
                 retval = True
+            # Specical case for s390x (s390x.zVM, s390x.zKVM)
+            #
+            elif s.arch.startswith(installed_arch) and installed_arch == 's390x':
+                retval = True
             else:
                 error("")
                 error("*** ERROR:")
